a3c150b0fd7f9fe1f738300807be33d158068062
Jesse de Wit <witdejesse@hotmail.com>
2025-06-25 11:36:36 +0200

    move select_leaves into TreeService

--- crates/spark/src/tree/service.rs
+++ crates/spark/src/tree/service.rs
@@ -9,14 +9,33 @@ impl TreeService {
         TreeService { state }
     }
 
-    pub fn select_leaves(&self, amount: u64) -> Result<Vec<TreeNode>, TreeServiceError> {
-        let selected_leaves = self.state.select_leaves(amount);
-        match selected_leaves {
-            Ok(leaves) => Ok(leaves),
-            Err(e) => {
-                //TODO We should as the ssp to swap at this point
-                Err(e)
+    pub async fn select_leaves(
+        &self,
+        target_amount: u64,
+    ) -> Result<Vec<TreeNode>, TreeServiceError> {
+        if target_amount == 0 {
+            return Err(TreeServiceError::IllegalAmount);
+        }
+
+        let mut amount = 0;
+        let mut nodes = vec![];
+        let mut leaves = self.state.get_leaves().await;
+        leaves.sort_by(|a, b| b.value.cmp(&a.value));
+
+        let mut aggregated_amount: u64 = 0;
+        for leaf in leaves {
+            aggregated_amount += leaf.value;
+            if target_amount.saturating_sub(amount) >= leaf.value {
+                amount += leaf.value;
+                nodes.push(leaf);
             }
         }
+        if amount < target_amount {
+            match aggregated_amount > target_amount {
+                true => return Err(TreeServiceError::UnselectableAmount),
+                false => return Err(TreeServiceError::InsufficientFunds),
+            }
+        }
+        Ok(nodes)
     }
 }
--- crates/spark/src/tree/state.rs
+++ crates/spark/src/tree/state.rs
@@ -1,9 +1,9 @@
-use std::sync::Mutex;
+use tokio::sync::Mutex;
 
-use super::{TreeNode, error::TreeServiceError};
+use super::TreeNode;
 
 pub struct TreeState {
-    pub leaves: Mutex<Vec<TreeNode>>,
+    leaves: Mutex<Vec<TreeNode>>,
 }
 
 impl TreeState {
@@ -13,30 +13,7 @@ impl TreeState {
         }
     }
 
-    pub fn select_leaves(&self, target_amount: u64) -> Result<Vec<TreeNode>, TreeServiceError> {
-        if target_amount == 0 {
-            return Err(TreeServiceError::IllegalAmount);
-        }
-
-        let mut amount = 0;
-        let mut nodes = vec![];
-        let mut leaves = self.leaves.lock().unwrap().clone();
-        leaves.sort_by(|a, b| b.value.cmp(&a.value));
-
-        let mut aggregated_amount: u32 = 0;
-        for leaf in leaves {
-            aggregated_amount += leaf.value;
-            if target_amount.saturating_sub(amount) >= leaf.value {
-                amount += leaf.value;
-                nodes.push(leaf);
-            }
-        }
-        if amount < target_amount {
-            match aggregated_amount > target_amount {
-                true => return Err(TreeServiceError::UnselectableAmount),
-                false => return Err(TreeServiceError::InsufficientFunds),
-            }
-        }
-        Ok(nodes)
+    pub async fn get_leaves(&self) -> Vec<TreeNode> {
+        self.leaves.lock().await.clone()
     }
 }
