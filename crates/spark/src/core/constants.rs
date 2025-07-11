7f682f481b72d987680e383f5a8b559312458096
Jesse de Wit <witdejesse@hotmail.com>
2025-07-11 09:46:44 +0200

    sequence number tests

--- crates/spark/src/core/constants.rs
+++ crates/spark/src/core/constants.rs
@@ -39,3 +39,49 @@ fn to_sequence(blocks: u16) -> Sequence {
     let new_locktime = LockTime::Blocks(Height::from_height(blocks));
     Sequence::from_consensus(new_locktime.to_consensus_u32() | SPARK_SEQUENCE_FLAG)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_initial_sequence() {
+        let sequence = initial_sequence();
+        assert!(sequence.is_height_locked());
+        assert!(sequence.is_relative_lock_time());
+        assert!(!sequence.is_time_locked());
+        let locktime = sequence.to_relative_lock_time().unwrap();
+        assert!(locktime.is_block_height());
+        let LockTime::Blocks(height) = locktime else {
+            panic!("Expected a block height locktime");
+        };
+
+        assert_eq!(height.value(), INITIAL_TIME_LOCK);
+    }
+
+    #[test]
+    fn test_next_sequence() {
+        let mut sequence = initial_sequence();
+
+        for i in 1u16..20 {
+            let next = next_sequence(sequence);
+            let next = next.unwrap();
+            assert!(next.is_height_locked());
+            assert!(next.is_relative_lock_time());
+
+            let LockTime::Blocks(height) = next.to_relative_lock_time().unwrap() else {
+                panic!("Expected a block height locktime");
+            };
+            assert_eq!(height.value(), INITIAL_TIME_LOCK - i * TIME_LOCK_INTERVAL);
+            sequence = next;
+        }
+
+        let LockTime::Blocks(height) = sequence.to_relative_lock_time().unwrap() else {
+            panic!("Expected a block height locktime");
+        };
+
+        assert_eq!(height.value(), TIME_LOCK_INTERVAL);
+        let next = next_sequence(sequence);
+        assert!(next.is_none());
+    }
+}
