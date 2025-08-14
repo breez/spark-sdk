feefe9e944a043ea3a3c97cdb372c3e42f55959a
Jesse de Wit <witdejesse@hotmail.com>
2025-08-14 18:54:27 +0200

    simplify retry channel implementation

--- crates/spark/src/operator/rpc/transport/retry_channel.rs
+++ crates/spark/src/operator/rpc/transport/retry_channel.rs
@@ -8,9 +8,17 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
-use tonic::{Status, body::BoxBody, transport::Error as TransportError};
+use tonic::{Status, body::BoxBody};
 use tower_service::Service;
 use tracing::{debug, trace};
+
+#[derive(Debug, thiserror::Error)]
+pub enum RetryChannelError {
+    #[error("gRPC status error: {0}")]
+    Status(#[from] Status),
+    #[error("transport error: {0}")]
+    Transport(#[from] tonic::transport::Error),
+}
 
 /// A channel that retries a gRPC call once if a transport error occurs
 #[derive(Debug, Clone)]
@@ -29,86 +37,56 @@ where
 
 impl<T, ResBody> Service<Request<BoxBody>> for RetryChannel<T>
 where
-    T: Service<Request<BoxBody>, Response = Response<ResBody>> + Clone + Send + 'static,
+    T: Service<Request<BoxBody>, Response = Response<ResBody>, Error = tonic::transport::Error>
+        + Clone
+        + Send
+        + 'static,
     T::Future: Send + 'static,
-    T::Error: std::error::Error + Send + Sync + 'static,
     ResBody: Body<Data = Bytes> + Send + 'static,
     <ResBody as Body>::Error: Into<Box<dyn std::error::Error + Send + Sync>> + Send,
 {
     type Response = Response<ResBody>;
-    type Error = T::Error;
+    type Error = RetryChannelError;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
+        self.inner
+            .poll_ready(cx)
+            .map_err(RetryChannelError::Transport)
     }
 
     fn call(&mut self, req: Request<BoxBody>) -> Self::Future {
-        // Prepare the request body first
-        let (head, mut body) = req.into_parts();
-        let pinned_body = Pin::new(&mut body);
-        let mut context = Context::from_waker(futures::task::noop_waker_ref());
-
-        // Attempt to read the request, in order to create two copies of the request below.
-        let poll = BoxBody::poll_frame(pinned_body, &mut context);
-        let maybe_data = match poll {
-            Poll::Ready(Some(Ok(frame))) => Some(frame.into_data().unwrap()),
-            _ => None,
-        };
-
-        // Create two copies of the request if possible.
-        let (original_req, maybe_retry_req) = if let Some(data) = maybe_data {
-            let full_body =
-                http_body_util::Full::new(data).map_err(|_| Status::internal("infallible error"));
-
-            (
-                Request::from_parts(head.clone(), BoxBody::new(full_body.clone())),
-                Some(Request::from_parts(head, BoxBody::new(full_body))),
-            )
-        } else {
-            // Can't create retry request, just use the original body
-            (Request::from_parts(head, body), None)
-        };
-
         // Clone the inner service for both initial call and potential retry
-        let mut inner_clone_for_initial = self.inner.clone();
-        let mut inner_clone_for_retry = self.inner.clone();
+        let mut inner_clone = self.inner.clone();
+
+        // Prepare the request body first
+        let (head, body) = req.into_parts();
 
         Box::pin(async move {
+            let data = body.collect().await?.to_bytes();
+            let full_body =
+                http_body_util::Full::new(data).map_err(|_| Status::internal("infallible error"));
+            let original_req = Request::from_parts(head.clone(), BoxBody::new(full_body.clone()));
+            let retry_req = Request::from_parts(head, BoxBody::new(full_body));
+
             // Wait for the initial service to be ready and make the call
-            poll_fn(|cx| inner_clone_for_initial.poll_ready(cx)).await?;
-            let res = inner_clone_for_initial.call(original_req).await;
+            poll_fn(|cx| inner_clone.poll_ready(cx)).await?;
+            let res = inner_clone.call(original_req).await;
 
-            // Early return if call succeeded or retry_req is None
-            let retry_req = match (&res, maybe_retry_req) {
-                (Ok(_), _) => return res,
-                (_, None) => return res,
-                (_, Some(req)) => req,
+            let err = match res {
+                Ok(ok) => return Ok(ok),
+                Err(e) => e,
             };
 
-            // Check if the error is a transport error
-            match &res {
-                Err(e) => {
-                    // Use the standard error reflection pattern
-                    let err_ref: &(dyn std::error::Error + 'static) =
-                        e as &(dyn std::error::Error + 'static);
-                    if !err_ref.is::<TransportError>() {
-                        trace!("RetryChannel: non-transport error detected, not retrying");
-                        return res;
-                    }
-
-                    debug!(
-                        "RetryChannel: transport error detected: {:?}, retrying...",
-                        e
-                    );
-                }
-                _ => return res,
-            };
+            debug!(
+                "RetryChannel: transport error detected: {:?}, retrying...",
+                err
+            );
 
             // Wait for the retry clone to be ready and make the call
-            poll_fn(|cx| inner_clone_for_retry.poll_ready(cx)).await?;
+            poll_fn(|cx| inner_clone.poll_ready(cx)).await?;
             trace!("RetryChannel: making retry call");
-            inner_clone_for_retry.call(retry_req).await
+            Ok(inner_clone.call(retry_req).await?)
         })
     }
 }
