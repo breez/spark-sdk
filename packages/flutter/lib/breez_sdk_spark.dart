39447cd931979b65f9a07a894956d062d0ca92f5
Jesse de Wit <witdejesse@hotmail.com>
2025-09-23 20:20:22 +0200

    add copyWith from the docs to flutter config

--- packages/flutter/lib/breez_sdk_spark.dart
+++ packages/flutter/lib/breez_sdk_spark.dart
@@ -1,5 +1,7 @@
 library;
 
+import 'package:breez_sdk_spark_flutter/src/rust/models.dart';
+
 export 'src/rust/frb_generated.dart' show BreezSdkSparkLib;
 export 'src/rust/errors.dart';
 export 'src/rust/events.dart';
@@ -7,3 +9,21 @@ export 'src/rust/logger.dart';
 export 'src/rust/models.dart';
 export 'src/rust/sdk_builder.dart';
 export 'src/rust/sdk.dart';
+
+extension SDKConfig on Config {
+  Config copyWith({
+  String? apiKey,
+  Network? network,
+  int? syncIntervalSecs,
+  Fee? maxDepositClaimFee,
+  String? lnurlDomain,
+  }) {
+    return Config(
+      apiKey: apiKey ?? this.apiKey,
+      network: network ?? this.network,
+      syncIntervalSecs: syncIntervalSecs ?? this.syncIntervalSecs,
+      maxDepositClaimFee: maxDepositClaimFee ?? this.maxDepositClaimFee,
+      lnurlDomain: lnurlDomain ?? this.lnurlDomain,
+    );
+  }
+}
