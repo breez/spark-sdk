c490579ee62276585ace00c16b6da9c20afc3504
Roei Erez <roeierez@gmail.com>
2025-09-30 22:57:29 +0300

    fix flutter

--- packages/flutter/rust/src/models.rs
+++ packages/flutter/rust/src/models.rs
@@ -83,7 +83,9 @@ pub enum _Fee {
 }
 
 #[frb(mirror(GetInfoRequest))]
-pub struct _GetInfoRequest {}
+pub struct _GetInfoRequest {
+    pub force_sync: bool,
+}
 
 #[frb(mirror(GetInfoResponse))]
 pub struct _GetInfoResponse {
