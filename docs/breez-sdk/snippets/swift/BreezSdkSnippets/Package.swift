1b0d74b8875cdd50d64e81deaf2c1d4d8bd6a92f
Daniel Granhão <daniel.granhao@gmail.com>
2025-10-10 14:30:34 +0100

    Fix Package.swift comments

--- docs/breez-sdk/snippets/swift/BreezSdkSnippets/Package.swift
+++ docs/breez-sdk/snippets/swift/BreezSdkSnippets/Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     platforms: [.macOS("15.0")],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-        //.package(url: "https://github.com/breez/breez-sdk-spark-swift", exact: "0.1.3")
-        // To use a local version of breez-sdk-spark, comment-out the above and un-comment:
         .package(
             name: "bindings-swift", path: "../../../../../crates/breez-sdk/bindings/langs/swift"),
+        // To use a published version of breez-sdk-spark, comment-out the above and un-comment:
+        //.package(url: "https://github.com/breez/breez-sdk-spark-swift", exact: "0.2.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,9 +19,9 @@ let package = Package(
         .executableTarget(
             name: "BreezSdkSnippets",
             dependencies: [
-                //.product(name: "BreezSdkSpark", package: "breez-sdk-spark-swift")
-                // To use a local version of breez-sdk-spark, comment-out the above and un-comment:
                 .product(name: "BreezSdkSpark", package: "bindings-swift")
+                // To use a published version of breez-sdk-spark, comment-out the above and un-comment:
+                //.product(name: "BreezSdkSpark", package: "breez-sdk-spark-swift")
             ],
             path: "Sources",
             linkerSettings: [
