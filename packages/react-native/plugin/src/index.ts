cf92e54e5177b3c9290410c24ad77b2fa638deeb
Ross Savage <mr.dangeross@googlemail.com>
2025-12-15 12:55:58 +0100

    Include binaries in package

--- packages/react-native/plugin/src/index.ts
+++ packages/react-native/plugin/src/index.ts
@@ -3,28 +3,19 @@ import {
   withPlugins,
   createRunOncePlugin,
 } from '@expo/config-plugins';
-import { withBinaryArtifacts } from './withBinaryArtifacts';
 import { withBreezSdkAndroid } from './withAndroid';
 import { withBreezSdkIOS } from './withIOS';
 import { sdkPackage } from './utils';
 
-export type BreezSdkPluginOptions = {
-  /**
-   * Skip downloading binary artifacts (default: false)
-   * Set to true if you want to handle binary downloads manually
-   */
-  skipBinaryDownload?: boolean;
-};
+export type BreezSdkPluginOptions = {};
 
 const withBreezSdk: ConfigPlugin<BreezSdkPluginOptions | void> = (
   config,
   options
 ) => {
-  const { skipBinaryDownload = false } = options || {};
+  const {} = options || {};
 
   return withPlugins(config, [
-    // Download binary artifacts first
-    ...(skipBinaryDownload ? [] : [withBinaryArtifacts]),
     // Configure Android
     withBreezSdkAndroid,
     // Configure iOS
